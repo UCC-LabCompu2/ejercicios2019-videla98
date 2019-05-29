8d85ebc765805a2320faf7e6356cadfbcfbae0d2
agus98 <agustinvidela0198@gmail.com>
2019-05-29 09:49:56 -0300

    Hecho ejercicio mostrar_ocultar

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -1,7 +1,14 @@
 /**
  * Created by Agus on 6/5/2017.
  */
-
+function mostrar_ocultar_div(id_elem){
+    if(id_elem == "ocultarDiv"){
+        document.getElementById("unDiv").style.display = 'none';
+    }
+    if(id_elem == "mostrarDiv"){
+        document.getElementById("unDiv").style.display = 'block';
+    }
+}
 /**
  * Descripción
  * @method Nombre de la función
